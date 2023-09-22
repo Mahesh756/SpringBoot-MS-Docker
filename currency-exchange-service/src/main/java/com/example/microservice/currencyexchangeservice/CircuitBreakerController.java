42f736a1e5802005656990e93ddec580c4b0b0ad
mahesh <maheshmaahi67@gmail.com>
2023-09-22 15:35:43 +0530

    adding docker files

---
+++ currency-exchange-service/src/main/java/com/example/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,35 @@
+package com.example.microservice.currencyexchangeservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+
+	private Logger logger = 
+			LoggerFactory.getLogger(CircuitBreakerController.class);
+	
+	@GetMapping("/sample-api")
+//	@Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+//	@CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name="default")
+	public String sampleApi() {
+		logger.info("Sample API Call received");
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",String.class);
+//		return forEntity.getBody();
+		return "sample-api";
+	}
+	
+	
+	public String hardcodedResponse(Exception ex) {
+		return "fallback-Response";
+	}
+}
